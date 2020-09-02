9fda937a015934795c0213e826d44245db1fb55e
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-09-02 15:11:18 +0200

    EX 2

---
+++ indexEx2.php
@@ -0,0 +1,26 @@
+<?php
+
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+
+$isNameLongEnough = strlen ($name) > 3;
+
+$isDotPresent = strpos ( $mail , ".");
+$isAtPresent = strpos ( $mail , "@");
+$isValidMail = ($isDotPresent && $isAtPresent);
+
+$IsAgeNumber = is_numeric ($age)
+
+ ?>
+
+<h2>
+<?php
+  if ($isNameLongEnough && $isValidMail && $IsAgeNumber) {
+    echo "ACCESSO RIUSCITO";
+  }
+  else {
+    echo "ACCESSO NEGATO";
+  }
+   ?>
+</h2>
